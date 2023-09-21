4a14c87834a08971d3fb698ee361d365758a69fa
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2023-09-20 22:11:52 -0300

    Commit teste

---
+++ src/Pacote_Teste/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pacote_Teste;
+
+/**
+ *
+ * @author marce
+ */
+public class NewClass {
+    
+}
